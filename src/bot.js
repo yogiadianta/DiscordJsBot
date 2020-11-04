fa81255a9d1bf2935dafeea30dfd34ec7a661ca7
Yogi Adianta <yogiadianta0@gmail.com>
2020-11-04 15:08:52 +0700

    Listening and reply on message function

--- src/bot.js
+++ src/bot.js
@@ -3,4 +3,17 @@ require('dotenv').config();
 const { Client } = require('discord.js');
 const client = new Client();
 
+client.on('ready', () => {
+  console.log(`${client.user.tag} has logged in.`);
+});
+
+client.on('message', (message) => {
+  if (message.author.bot) return;
+  console.log(`[${message.author.tag}]: ${message.content}`);
+  if (message.content === 'hello') {
+    // message.reply('hello there');
+    message.channel.send('hello');
+  }
+})
+
 client.login(process.env.DISCORDJS_BOT_TOKEN);
