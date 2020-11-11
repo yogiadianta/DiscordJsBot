24cf7b61abeaf19acb3c918a2b5e969e94352e62
Yogi Adianta <yogiadianta0@gmail.com>
2020-11-11 17:43:09 +0700

    Add kick command

--- src/bot.js
+++ src/bot.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const { Client } = require('discord.js');
 const client = new Client();
+const PREFIX = "!";
 
 client.on('ready', () => {
   console.log(`${client.user.tag} has logged in.`);
@@ -9,10 +10,18 @@ client.on('ready', () => {
 
 client.on('message', (message) => {
   if (message.author.bot) return;
-  console.log(`[${message.author.tag}]: ${message.content}`);
-  if (message.content === 'hello') {
-    // message.reply('hello there');
-    message.channel.send('hello');
+  if (message.content.startsWith(PREFIX)) {
+    const [CMD_NAME, ...args] = message.content
+      .trim()
+      .substring(PREFIX.length)
+      .split(/\s+/);
+    
+    // console.log(CMD_NAME);
+    // console.log(args);
+    if (CMD_NAME === 'kick') {
+      message.channel.send('Kicked the User');
+    }
+    // console.log('Kicked the User');
   }
 })
 
